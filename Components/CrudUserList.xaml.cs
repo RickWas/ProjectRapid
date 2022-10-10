6b8d533f7e430f3be37f951db8c83b7d0ce21d0e
RickWas <hjrick74@gmail.com>
2022-10-10 08:26:01 +0200

    Add project files.

---
+++ Components/CrudUserList.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MVVM_CRUD_App_Rapid.Components
+{
+    /// <summary>
+    /// Interaction logic for CrudUserList.xaml
+    /// </summary>
+    public partial class CrudUserList : UserControl
+    {
+        public CrudUserList()
+        {
+            InitializeComponent();
+        }
+    }
+}
